6fc1cd5c792240525939745e21111fefc9c5b8ac
NourG8 <nourgerfali08@gmail.com>
2021-11-21 21:44:06 +0100

    fist-commit

---
+++ src/main/java/iset/example/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package iset.example.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class ResourceNotFoundException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
